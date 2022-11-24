e9146ba9e1ac1ebc7933717ba5e214365bce0496
zGonzq <116316238+zGonzq@users.noreply.github.com>
2022-11-24 20:46:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var mascotas= ["Pedrito", "Rufo", "Chancla"]
+console.log("Mis mascotas son", mascotas, "Les quiero mucho.")
+
+var mascota1 = prompt("Dime el nombre de tu mascota")
+var mascota2 = prompt("Dime el nombre de tu otra mascota")
+console.log("Quiero mucho a mis mascotas", mascota1, "y", mascota2)
+
+var texto= "100"
+console.log (texto)
+
+console.log(typeof texto)
+console.log(parseFloat(texto))
